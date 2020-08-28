5cd69cdb7822c6bfc70f2c15872e29aa25605cc0
Juan Pampa <juanjose201626@gmail.com>
2020-08-27 21:41:20 -0500

    Chat

---
+++ public/chat.js
@@ -0,0 +1,38 @@
+const socket = io();
+
+// dom elements
+let message = document.getElementById("message");
+let username = document.getElementById("username");
+let btn = document.getElementById("send");
+let output = document.getElementById("output");
+let actions = document.getElementById("actions");
+
+btn.addEventListener('click',function(){
+    socket.emit('chat:message',{
+        username: username.value, 
+        message: message.value
+    });
+});
+
+
+message.addEventListener('keypress',function(){
+    socket.emit('chat:typing', username.value);
+});
+
+socket.on('chat:message', function(data){
+    actions.innerHTML = '';
+    output.innerHTML += `
+    <p>
+        <strong>${data.username}</strong>: ${data.message}
+    </p>
+    `;
+})
+
+socket.on('chat:typing', function(data){
+    console.log(data);
+    actions.innerHTML = `
+    <p>
+        <em>${data} esta escribiendo un mensaje</em>
+    </p>
+    `;
+})
